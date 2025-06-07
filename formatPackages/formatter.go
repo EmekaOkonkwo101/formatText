a33cc31a24d3e773500269668d953ce99b6ae5cc
emekaokonkwo101 <okonkwoemeka1000@gmail.com>
2025-06-07 18:13:04 +0100

    Add GetTextInBoldFormat function for bold text formatting

--- formatPackages/formatter.go
+++ formatPackages/formatter.go
@@ -7,23 +7,21 @@ import (
 )
 
 func GetTextInBoldFormat(text string) string {
-	mapping := map[rune]rune{}
-	normal := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-	bolded := "𝗔𝗕𝗖𝗗𝗘𝗙𝗚𝗛𝗜𝗝𝗞𝗟𝗠𝗡𝗢𝗣𝗤𝗥𝗦𝗧𝗨𝗩𝗪𝗫𝗬𝗭𝗮𝗯𝗰𝗱𝗲𝗳𝗴𝗵𝗶𝗷𝗸𝗹𝗺𝗻𝗼𝗽𝗾𝗿𝘀𝘁𝘂𝘃𝘄𝘅𝘆𝘇𝟬𝟭𝟮𝟯𝟰𝟱𝟲𝟳𝟴𝟵"
+	replacer := strings.NewReplacer(
+		"A", "𝗔", "B", "𝗕", "C", "𝗖", "D", "𝗗", "E", "𝗘", "F", "𝗙",
+		"G", "𝗚", "H", "𝗛", "I", "𝗜", "J", "𝗝", "K", "𝗞", "L", "𝗟",
+		"M", "𝗠", "N", "𝗡", "O", "𝗢", "P", "𝗣", "Q", "𝗤", "R", "𝗥",
+		"S", "𝗦", "T", "𝗧", "U", "𝗨", "V", "𝗩", "W", "𝗪", "X", "𝗫",
+		"Y", "𝗬", "Z", "𝗭", "a", "𝗮", "b", "𝗯", "c", "𝗰", "d", "𝗱",
+		"e", "𝗲", "f", "𝗳", "g", "𝗴", "h", "𝗵", "i", "𝗶", "j", "𝗷",
+		"k", "𝗸", "l", "𝗹", "m", "𝗺", "n", "𝗻", "o", "𝗼", "p", "𝗽",
+		"q", "𝗾", "r", "𝗿", "s", "𝘀", "t", "𝘁", "u", "𝘂", "v", "𝘃",
+		"w", "𝘄", "x", "𝘅", "y", "𝘆", "z", "𝘇", "0", "𝟬", "1", "𝟭",
+		"2", "𝟮", "3", "𝟯", "4", "𝟰", "5", "𝟱", "6", "𝟲", "7", "𝟳",
+		"8", "𝟴", "9", "𝟵",
+	)
 
-	for i, char := range normal {
-		mapping[char] = rune(bolded[i])
-	}
-
-	var builder strings.Builder
-	for _, char := range text {
-		if boldChar, exists := mapping[char]; exists {
-			builder.WriteRune(boldChar)
-		} else {
-			builder.WriteRune(char)
-		}
-	}
-	return builder.String()
+	return replacer.Replace(text)
 }
 
 
